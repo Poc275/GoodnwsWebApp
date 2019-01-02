a68660d238ae7fce7fd5441e3d3c3426acf9b26a
Peter O'Connor <poc275@gmail.com>
2019-01-02 09:30:33 +0000

    Updated home page view to show all stories from the db.

--- GoodnwsWebApp/Startup.cs
+++ GoodnwsWebApp/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using GoodnwsWebApp.Models;
+using GoodnwsWebApp.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -33,10 +34,10 @@ namespace GoodnwsWebApp
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddDbContext<GoodnwsDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Goodnws")));
+            services.AddScoped<IStories, Stories>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ GoodnwsWebApp/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using GoodnwsWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoodnwsWebApp.ViewModels
+{
+    public class HomePageViewModel
+    {
+        public IQueryable<Story> Stories { get; set; }
+    }
+}
