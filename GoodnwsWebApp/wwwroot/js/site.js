a006ff22f4e14c43f1487f0c5a6f18a0f581b136
Peter O'Connor <poc275@gmail.com>
2019-01-03 17:56:13 +0000

    Article markup moved to separate partial views.

--- GoodnwsWebApp/wwwroot/js/site.js
+++ GoodnwsWebApp/wwwroot/js/site.js
@@ -1,8 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-fetch('/Home/GetTotalNumberOfArticles').then(function (response) {
+﻿fetch('/Home/GetTotalNumberOfArticles').then(function (response) {
     return response.json();
 }).then(function (total) {
     nPages = Math.ceil(total / 10);
